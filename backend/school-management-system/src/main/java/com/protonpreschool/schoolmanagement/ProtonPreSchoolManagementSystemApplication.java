ab194e7b7a0f6cdac9ec18cadb16b77433fceccb
Divyansh Dixit <114280477+divyanshdixit10@users.noreply.github.com>
2025-03-31 15:59:48 +0530

    Initial Spring Boot project setup

---
+++ backend/school-management-system/src/main/java/com/protonpreschool/schoolmanagement/ProtonPreSchoolManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.protonpreschool.schoolmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtonPreSchoolManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtonPreSchoolManagementSystemApplication.class, args);
+	}
+
+}
